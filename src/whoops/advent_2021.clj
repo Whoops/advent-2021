ac12c350e7a1d0ad5ce397f7ba1b765d00674584
Walton Hoops <me@waltonhoops.com>
2021-12-02 08:25:10 -0700

    day 2

--- src/whoops/advent_2021.clj
+++ src/whoops/advent_2021.clj
@@ -34,3 +34,25 @@
                     (> (+ b c d) (+ a b c)) [(inc cnt) b c d]
                     :else [cnt b c d]))]
     (first (reduce reducer [0 nil nil nil] data))))
+
+(defn p2-1 []
+  (let [data (map #(string/split % #" ")  (read-input "2-1"))
+        reducer (fn [[x y] [direction distance]]
+                  (let [distance (Integer/parseInt distance)]
+                    (condp = direction
+                      "forward" [(+ x distance) y]
+                      "down" [x (+ y distance)]
+                      "up" [x (- y distance)])))
+        [x y] (reduce reducer [0 0] data)]
+    (* x y)))
+
+(defn p2-2 []
+  (let [data (map #(string/split % #" ")  (read-input "2-1"))
+        reducer (fn [[x y aim] [direction distance]]
+                  (let [distance (Integer/parseInt distance)]
+                    (condp = direction
+                      "forward" [(+ x distance) (+ y (* aim distance)) aim]
+                      "down" [x y (+ aim distance)]
+                      "up" [x y (- aim distance)])))
+        [x y _aim] (reduce reducer [0 0 0] data)]
+    (* x y)))
